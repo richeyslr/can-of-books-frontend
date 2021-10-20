84472f560ed1000ae1e8fa136f5d764521caff9a
Spencer Richey <richeyslr@gmail.com>
2021-10-19 21:41:26 -0500

    BestBooks componentcreated using jwt

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,31 +1,40 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
+  componentDidMount = () => {
+    if(this.props.auth0.isAuthenticated) {
+      this.props.auth0.getIdTokenClaims()
+      .then(res => {
+        const jwt = res.__raw;
+        const config = {
+          headers: {"Authorization" : `Bearer ${jwt}`},
+          method: 'get',
+          baseURL: "http://localhost:3001/",
+          // baseURL: process.env.REACT_APP_SERVER,
+          url: '/test'
+        }
+        axios(config)
+          .then(axiosResults => console.log(axiosResults.data))
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
-
   render() {
-
-    /* TODO: render user's books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
+    return(
+      <Jumbotron>
+        <h1>My Favorite Books</h1>
+        <p>
+          This is a collection of my favorite books
+        </p>
+      </Jumbotron>
     )
   }
 }
 
-export default BestBooks;
+export default withAuth0(BestBooks);
