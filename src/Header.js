0ee444f78589dd5f38fe8c9dbe594a9eddde2e9f
Spencer Richey <richeyslr@gmail.com>
2021-10-19 21:09:24 -0500

    header and logout working

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Navbar, NavItem } from 'react-bootstrap';
+import LogoutButton from './LogoutButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavItem }  from 'react-bootstrap';
+import LoginButton from './LoginButton';
+import { withAuth0 } from '@auth0/auth0-react';
 import { Link } from "react-router-dom";
 import './Header.css';
 
 class Header extends React.Component {
   render() {
-    return (
+    return(
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* TODO: if the user is logged in, render a navigation link to profile page */}
-        {/* TODO: if the user is logged in, render the `LogoutButton` */}
+        <NavItem><Link to="/profile" className="nav-link">Profile</Link></NavItem>
+        {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
       </Navbar>
     )
   }
 }
 
-export default Header;
+export default withAuth0(Header);
