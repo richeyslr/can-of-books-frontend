486dde84c07e659003c429b8eaf1f36b05b38382
Spencer Richey <richeyslr@gmail.com>
2021-10-19 20:43:17 -0500

    0auth installed and login components made

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <App />,
+  <Auth0Provider
+    domain="dev-fztieqgp.us.auth0.com"
+    clientId="uwItVVfIYvoSgzvhZBGtveYzzxz6dUtL"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById("root")
 );
